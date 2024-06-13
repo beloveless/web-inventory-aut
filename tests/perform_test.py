48c68dd6906f5a7b34e8039674e90e893fcf044a
sodamnloveless <129027879+beloveless@users.noreply.github.com>
2024-06-13 11:13:50 +0700

    tes

--- tests/perform_test.py
+++ tests/perform_test.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class BatikWebsiteTestCase(unittest.TestCase):
+class InventoryWebsiteTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         option = webdriver.FirefoxOptions()
