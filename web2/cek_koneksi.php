f948cdbf401a057965eb6f5e06afca0dea02d386
sodamnloveless <129027879+beloveless@users.noreply.github.com>
2024-07-17 10:50:01 +0700

    cekk

---
+++ web2/cek_koneksi.php
@@ -0,0 +1,22 @@
+<?php
+
+function pdo_connect(){
+    $DATABASE_HOST = 'mysql-db';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = 'password';
+    $DATABASE_NAME = 'inventory';
+    try {
+        echo "Connected to MySQL successfully";
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME, $DATABASE_USER, $DATABASE_PASS);
+    } catch(Exception $e) {
+        return $e;
+    }    
+}
+
+var_dump(pdo_connect());
+if ( pdo_connect() ) {
+    echo "connected";
+} else {
+    echo "failed to connect";
+}
+?>
