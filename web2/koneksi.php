9cc5f1fe24369a7f135f8d0e380b13667d80433b
sodamnloveless <129027879+beloveless@users.noreply.github.com>
2024-07-17 10:44:59 +0700

    cek koneksi

--- web2/koneksi.php
+++ web2/koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost", "root", "", "inventory");
+$koneksi = mysqli_connect("mysql-db", "root", "password", "inventory");
 
 if (mysqli_connect_errno()) {
 	echo "Koneksi gagal".mysqli_connect_error();
