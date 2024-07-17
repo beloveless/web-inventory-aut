624f4cdece4c097114c5b81fc11da50a91997ab0
sodamnloveless <129027879+beloveless@users.noreply.github.com>
2024-07-17 10:56:25 +0700

    coba

--- web2/cek_koneksi.php
+++ web2/cek_koneksi.php
@@ -15,8 +15,23 @@ function pdo_connect(){
 
 var_dump(pdo_connect());
 if ( pdo_connect() ) {
-    echo "connected";
+    echo "connected\n";
+
+    try {
+        $stmt = $pdo->query("SELECT * FROM tb_admin");
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo "ID: " . $row['id_admin'] . "\n";
+            echo "Username: " . $row['username'] . "\n";
+            echo "Password: " . $row['password'] . "\n";
+        }
+    } catch (PDOException $e) {
+        echo 'Query failed: ' . $e->getMessage();
+    }
+
+    
 } else {
     echo "failed to connect";
 }
 ?>
+
